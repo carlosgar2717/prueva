51f57e06ae5c3c0a402ef366ac90085d58dd98d7
carlos <carlosgar27171@gmail.com>
2025-07-27 11:24:26 -0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "https://carlosgar2717.github.io/prueva",
-<<<<<<< HEAD
 });
-=======
-})
->>>>>>> 4559a3ca836c931fd10980290d0b5473e8ab323d
